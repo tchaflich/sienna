958cc5f8159bfe920c707d1b43fa0417affcf362
Thomas C. Haflich <tchaflich@gmail.com>
2019-07-27 11:40:23 -0400

    chore: work on build process
    * changed eslintrc file format, standardized rules
    * added cascading eslintrc for testing, some rules tbd
    * webpack scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/index.js',
+	entry: './src/js/index.js',
 
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
 
-  mode: 'none',
+	mode: 'none',
 
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
-      },
-      {
-        test: /test\.js$/,
-        use: {
-          loader: "mocha-loader"
-        },
-        exclude: /node_modules/
-      },
-    ]
-  },
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader', 'eslint-loader',],
+			},
+			{
+				test: /test\.js$/,
+				use: {
+					loader: 'mocha-loader',
+				},
+				exclude: /node_modules/,
+			},
+		],
+	},
 };
