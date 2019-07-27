e34dda4012c418f2df3aef0d071c44dbe22f5318
Thomas C. Haflich <tchaflich@gmail.com>
2019-07-27 18:26:04 -0400

    feature: adder component styling & QoL

--- src/js/components/Adder.js
+++ src/js/components/Adder.js
@@ -9,10 +9,12 @@ class Adder extends Component {
 
 		this.state = {
 			'inputValue': '',
+			'lastColor': null,
 		};
 
 		this.handleButtonClick = this.handleButtonClick.bind(this);
 		this.handleInputUpdate = this.handleInputUpdate.bind(this);
+		this.handleInputKeyDown = this.handleInputKeyDown.bind(this);
 	}
 
 	getColorObject(text) {
@@ -31,23 +33,68 @@ class Adder extends Component {
 		});
 	}
 
-	handleButtonClick() {
-		const color = this.getColorObject(this.state.inputValue);
-		if (color) {
-			this.props.onAdd(color);
+	handleInputKeyDown(event) {
+		if (event.keyCode === 13) {
+			try {
+				const color = this.getColorObject(this.state.inputValue);
+				this.addColor(color);
+			} catch (e) {
+				// shrug for now
+			}
 		}
 	}
 
+	handleButtonClick() {
+		const color = this.getColorObject(this.state.inputValue);
+		if (color) {
+			this.addColor(color);
+		}
+	}
+
+	addColor(color) {
+		this.setState({
+			'inputValue': '',
+			'lastColor': color,
+		});
+		this.props.onAdd(color);
+	}
+
 	render () {
+		let color;
+		try {
+			color = this.getColorObject(this.state.inputValue);
+		} catch (e) {
+			color = null;
+		}
+
+		if (!color && this.state.lastColor) {
+			color = this.state.lastColor;
+		}
+
+		let illumination = null;
+		if (color && color.getRelativeLuminance() > 0.6) {
+			illumination = 'dark';
+		} else {
+			illumination = 'light';
+		}
+
+		const styles = {
+			'backgroundColor': color ? color.getHexString() : null,
+		};
+
 		return (
-			<div className="Adder">
+			<div className="Adder" style={styles}>
 				<input
 					type="text"
+					placeholder="#c0ffee, #7ea, etc"
+					className={illumination}
 					value={this.state.inputValue}
 					onChange={this.handleInputUpdate}
+					onKeyDown={this.handleInputKeyDown}
 				/>
 
 				<button
+					className={illumination}
 					onClick={this.handleButtonClick}
 				>Add</button>
 			</div>
